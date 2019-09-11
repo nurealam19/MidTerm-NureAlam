949405d6cdc40456d8897a023b3feca6e88d1680
nurealam19 <mdnurealam165@gmail.com>
2019-09-10 22:12:16 -0400

    TuesDay_night_2nd Push

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -12,5 +12,17 @@ public class Permutation {
          * Write Java program to compute all Permutation of a String
          *
          */
+        permutation("","RAM");
+    }
+
+    public static void permutation(String prefix, String  str){
+        int n = str.length();
+        if(n==0){
+            System.out.println(prefix);
+        }else {
+            for(int i = 0; i<n;i++){
+                permutation(prefix+ str.charAt(i),str.substring(0,i)+ str.substring(i+1, n));
+            }
+        }
     }
 }
