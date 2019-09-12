6ecf091d424cd739e1183d3a2f85d627c01b4041
nurealam19 <mdnurealam165@gmail.com>
2019-09-11 22:35:41 -0400

    Wednesday_night_2 Push

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,5 +1,10 @@
 package datastructure;
 
+import databases.ConnectToSqlDB;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class UseArrayList {
 
 	public static void main(String[] args) {
@@ -9,8 +14,50 @@ public class UseArrayList {
 		 * Store all the sorted data into one of the databases.
 		 * 
 		 */
-	
+		ArrayList<Object> arrayList = new ArrayList<>();
 
+		arrayList.add(25);
+		arrayList.add(56);
+		arrayList.add(25);
+		arrayList.add(12);
+		arrayList.add(10);
+		arrayList.add(21);
+		arrayList.add(9);
+		printForEach(arrayList);
+		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
+
+//		try {
+//			connectToSqlDB.insertDataFromArrayToSqlTable(newArray, "tbl_lowestNumber", "SortingNumbers");
+//			arrayList = connectToSqlDB.readDataBase("tbl_lowestNumber", "SortingNumbers");
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		System.out.println("Data is reading from the Table (tbl_lowestNumber) and displaying to the console");
+//		for(String st:lowestValue){
+//			System.out.println(st);
+//		}
+
+
+
+		//arrayList.remove(3); // remove elements
+
+
+		//for(Integer num : arrayList){ // this is for each loop
+		//System.out.println((num));
+
+		//Iterator iterator = arrayList.iterator(); // use Iterator
+		//while (iterator.hasNext()){
+		//System.out.println(iterator.next());
+		//}
+
+		//}
+	}
+	public static void printForEach(ArrayList<Object>arrayList){
+		Iterator<Object> iterator  = arrayList.iterator();
+		while (iterator.hasNext()){
+			System.out.println(iterator.next());
+		}
 	}
 
-}
+	}
